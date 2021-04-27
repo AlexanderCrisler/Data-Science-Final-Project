0dcb4ccb4d0015d9df75aee2c3e3d26fe4ca49ea
Alexander Crisler <alexcrisler@gmail.com>
2021-04-26 21:53:31 -0500

    added final project and all data collection with most of the scrubbing done

--- webscrape.py
+++ webscrape.py
@@ -1,13 +1,11 @@
 import requests
 # import json
 
-url = "https://www.footballdb.com/fantasy-football/index.html?pos=QB%2CRB%2CWR%2CTE&yr=2020&wk=17&rules=1"
-yr = "2010"
-wk = "1"
+url = f"https://www.footballdb.com/fantasy-football/index.html?pos=QB%2CRB%2CWR%2CTE&yr={yr}&wk={wk}&rules=1"
 
 seasons = {}
 
-for yr in range(2010, 2021):
+for yr in range(2016, 2021):
     weeks = []
     for wk in range(1, 18):
         weeks.append(
